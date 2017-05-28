d3a919acbe0e000d0fea5dea17fe7feb3ad89e87
Thomas Volk <info@thomasvolk.de>
2017-05-28 13:11:28 +0200

    change to Long

--- scala/factors.scala
+++ scala/factors.scala
@@ -1,7 +1,7 @@
 import scala.math
 
-def factors(n: Int) = (1 until (math.sqrt(n).toInt + 1)).filter( (i) => n % i == 0).map( i => List(i, n / i)).flatten.toList.sorted
-def properDivisors(n: Int) = factors(n).reverse.drop(1).reverse
+def factors(n: Long) = (1L until (math.sqrt(n).toLong + 1L)).filter( (i) => n % i == 0).map( i => List(i, n / i)).flatten.toSet.toList.sorted
+def properDivisors(n: Long) = factors(n).reverse.drop(1).reverse
 
 val factorsTestData = Seq(
     ( 0, Seq())
@@ -30,5 +30,5 @@ assert( properDivisors(3) == Seq(1) )
 assert( properDivisors(4) == Seq(1, 2) )
 assert( properDivisors(6) == Seq(1, 2, 3) )
 
-assert( properDivisors(6).foldLeft(0)(_ + _) == 6 )
-assert( properDivisors(28).foldLeft(0)(_ + _) == 28 )
+assert( properDivisors(6).foldLeft(0L)(_ + _) == 6 )
+assert( properDivisors(28).foldLeft(0L)(_ + _) == 28 )
