428dc5061be9f4f4e6af484a27524295b13a0924
Thomas Volk <info@thomasvolk.de>
2017-08-22 22:54:45 +0200

    add properDivisors

--- elixir/lib/factors.ex
+++ elixir/lib/factors.ex
@@ -5,6 +5,9 @@ defmodule Factors do
   def factors(n) do
      factors(n, round(:math.sqrt(n)) + 1, [])
   end
+  def properDivisors(n) do
+     Enum.drop factors(n), -1
+  end
   defp factors(n, i, results) when i > 0 do
     case rem(n, i) do
       0 -> factors(n, i - 1, results ++ [i] ++ [round(n/i)])
--- elixir/test/factors_test.exs
+++ elixir/test/factors_test.exs
@@ -2,7 +2,7 @@ defmodule FactorsTest do
   use ExUnit.Case
   doctest Factors
 
-  test "factors for 6" do
+  test "factors for different numbers" do
     assert Factors.factors(0) == []
     assert Factors.factors(1) == [1]
     assert Factors.factors(2) == [1,2]
@@ -13,5 +13,15 @@ defmodule FactorsTest do
     assert Factors.factors(12) == [1, 2, 3, 4, 6, 12]
     assert Factors.factors(13) == [1, 13]
     assert Factors.factors(28) == [1, 2, 4, 7, 14, 28]
+  end
+
+  test "properDivisors for different numbers" do
+    assert Factors.properDivisors(0) == []
+    assert Factors.properDivisors(1) == []
+    assert Factors.properDivisors(2) == [1]
+    assert Factors.properDivisors(4) == [1,2]
+    assert Factors.properDivisors(6) == [1,2,3]
+    assert Factors.properDivisors(13) == [1]
+    assert Factors.properDivisors(28) == [1, 2, 4, 7, 14]
   end
 end
