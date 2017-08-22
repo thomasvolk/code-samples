ccf48bd9832a695860f725bc407821b939f47d15
Thomas Volk <info@thomasvolk.de>
2017-08-22 22:01:45 +0200

    bugfix and mor tests

--- elixir/test/factors_test.exs
+++ elixir/test/factors_test.exs
@@ -10,5 +10,8 @@ defmodule FactorsTest do
     assert Factors.factors(4) == [1,2,4]
     assert Factors.factors(5) == [1,5]
     assert Factors.factors(6) == [1,2,3,6]
+    assert Factors.factors(12) == [1, 2, 3, 4, 6, 12]
+    assert Factors.factors(13) == [1, 13]
+    assert Factors.factors(28) == [1, 2, 4, 7, 14, 28]
   end
 end
