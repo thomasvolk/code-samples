d0d44c8ea4bf5f214b5d869ddee75879993f7636
Thomas Volk <thomas.volk@trustedshops.com>
2018-10-25 13:51:13 +0200

    add history

--- go/goroutines.go
+++ go/goroutines.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"flag"
 	"fmt"
 	"sync"
@@ -21,45 +22,103 @@ func tick() int64 {
 	return now() - START_TIME
 }
 
-func workerRoutine(wg *sync.WaitGroup, i int, timeout time.Duration, workerBuffer chan int) {
-	defer wg.Done()
-	for v := range workerBuffer {
+type Worker struct {
+	waittime       time.Duration
+	workerBuffer   chan int
+	receiveHistory chan int64
+}
+
+func (w Worker) run(i int) {
+	for v := range w.workerBuffer {
 		if v == POISON_PILL {
 			break
 		} else {
-			fmt.Printf("[GR %d] ; %012d ; receive ; %d\n", i, tick(), v)
-			time.Sleep(timeout)
+			t := tick()
+			w.receiveHistory <- t
+			time.Sleep(w.waittime)
 		}
 	}
 }
 
-func historyRoutine() {
+type History struct {
+	timeframe      time.Duration
+	sendHistory    chan int64
+	receiveHistory chan int64
+	messagesTotal  int
+}
 
+func (h History) run() {
+	receivedTotal := 0
+	send := 0
+	received := 0
+	active := true
+	for active {
+		select {
+		case <-h.sendHistory:
+			send++
+		case <-h.receiveHistory:
+			received++
+			receivedTotal++
+		case <-time.After(h.timeframe):
+			var b bytes.Buffer
+			b.WriteString("|")
+			for i := 0; i < send; i++ {
+				b.WriteString("S")
+			}
+			for i := 0; i < received; i++ {
+				b.WriteString("R")
+			}
+			fmt.Println(b.String())
+			send = 0
+			received = 0
+			if receivedTotal >= h.messagesTotal {
+				active = false
+			}
+		}
+	}
 }
 
 func main() {
 	var worker int
-	var timeout time.Duration
+	var waittime time.Duration
+	var timeframe time.Duration
 	var messages int
 	var buffer int
 	flag.IntVar(&worker, "worker", 4, "worker ")
 	flag.IntVar(&buffer, "buffer", 3, "buffer ")
 	flag.IntVar(&messages, "messages", 10, "messages ")
-	flag.DurationVar(&timeout, "timeout", 1*time.Second, "timeout")
+	flag.DurationVar(&waittime, "waittime", 1*time.Second, "waittime")
+	flag.DurationVar(&timeframe, "timeframe", 100*time.Millisecond, "timeframe")
 	flag.Parse()
 
 	workerBuffer := make(chan int, buffer)
 	defer close(workerBuffer)
+	sendHistory := make(chan int64, messages)
+	defer close(sendHistory)
+	receiveHistory := make(chan int64, messages)
+	defer close(receiveHistory)
 
 	var workerWg sync.WaitGroup
 	workerWg.Add(worker)
 
+	var historyWg sync.WaitGroup
+	historyWg.Add(1)
+	go func(h History) {
+		defer historyWg.Done()
+		h.run()
+	}(History{timeframe, sendHistory, receiveHistory, messages})
+
 	for i := 0; i < worker; i++ {
-		go workerRoutine(&workerWg, i, timeout, workerBuffer)
+		w := Worker{waittime, workerBuffer, receiveHistory}
+		go func(w Worker) {
+			defer workerWg.Done()
+			w.run(i)
+		}(w)
 	}
 
 	for i := 0; i < messages; i++ {
-		fmt.Printf("[MAIN] ; %012d ; send ; %d\n", tick(), i)
+		t := tick()
+		sendHistory <- t
 		workerBuffer <- i
 	}
 
@@ -68,5 +127,5 @@ func main() {
 	}
 
 	workerWg.Wait()
-
+	historyWg.Wait()
 }
